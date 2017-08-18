163ea2156618b04f7001da33b52f0d43b32d05e4
electricman226 <electricman226@gmail.com>
2017-08-18 15:43:31 -0400

    Fix console clear.

--- Modjector/command_list.cpp
+++ Modjector/command_list.cpp
@@ -18,7 +18,20 @@ bool ListCommand( std::vector<std::string> args )
 
 bool ClearCommand( std::vector<std::string> args )
 {
-	std::cout.clear();
+	COORD topLeft = { 0, 0 };
+	HANDLE console = GetStdHandle( STD_OUTPUT_HANDLE );
+	CONSOLE_SCREEN_BUFFER_INFO screen;
+	DWORD written;
+
+	GetConsoleScreenBufferInfo( console, &screen );
+	FillConsoleOutputCharacterA(
+		console, ' ', screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	FillConsoleOutputAttribute(
+		console, FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_BLUE,
+		screen.dwSize.X * screen.dwSize.Y, topLeft, &written
+	);
+	SetConsoleCursorPosition( console, topLeft );
 	return true;
 }
 
